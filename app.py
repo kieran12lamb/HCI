58ee91896115f74703f16e89dc64881d6bdf576b
Kieran <kieran12lamb@live.com>
2018-11-20 15:21:03 +0000

    basic querying

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, render_template, abort, redirect, url_for, jsonify
 import json
+import urllib
 app = Flask(__name__)
+
+gpURL = 'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=34f02dbe-2827-47ae-821f-d529e26075cd&'
+prescriptionURL ='https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=592f3b58-2da8-4cc7-b4ab-e0eb0f16aa3f&'
 
 @app.route("/")
 def index():
+    fileobj = urllib.request.urlopen(gpURL+'limit=1&q=title:jones' )
+    print(fileobj.read())
     return render_template('index.html')
 
 

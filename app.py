6ddc9441edc2f8396c15308f3f04f0600ca5b462
Kieran <kieran12lamb@live.com>
2018-11-21 13:25:36 +0000

    querying

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import json
 import urllib
 app = Flask(__name__)
 
-gpURL = 'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=34f02dbe-2827-47ae-821f-d529e26075cd&'
-prescriptionURL ='https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=592f3b58-2da8-4cc7-b4ab-e0eb0f16aa3f&'
+gpURL = 'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=34f02dbe-2827-47ae-821f-d529e26075cd&q='
+prescriptionURL='https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=592f3b58-2da8-4cc7-b4ab-e0eb0f16aa3f&q='
 
 @app.route("/")
 def index():
@@ -13,10 +13,17 @@ def index():
 @app.route("/graphs/<drug>",methods = ['POST'])
 def graphs(drug):
     if drug == 'warfarin':
-        fileobj = urllib.request.urlopen(gpURL+'limit=1&q=BNFItemDescription:methadone' )
-        print(fileobj.read())
+        url= prescriptionURL+drug
+        prescriptionData = urllib.request.urlopen(url)
+        prescriptionData = json.loads(prescriptionData.read().decode('utf-8'))
+        return render_template('graphs.html',prescriptionData = prescriptionData)
     elif drug == 'methadone':
-        print('boop2')
+        url= prescriptionURL+drug
+        prescriptionData = urllib.request.urlopen(url)
+        prescriptionData = json.loads(prescriptionData.read().decode('utf-8'))
+        return render_template('graphs.html',prescriptionData = prescriptionData)
     else:
-        print('boop3')
-    return render_template('graphs.html')
+        url= prescriptionURL+drug
+        prescriptionData = urllib.request.urlopen(url)
+        prescriptionData = json.loads(prescriptionData.read().decode('utf-8'))
+        return render_template('graphs.html',prescriptionData = prescriptionData)

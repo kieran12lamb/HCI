60183a11c0a2099939faff6e48cb667c93b8911e
Kieran <kieran12lamb@live.com>
2018-11-26 21:14:01 +0000

    About page

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ def getPostcode(gpID):
 def index():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 @app.route("/graphs/<drug>",methods = ['POST'])
 def graphs(drug):
     if drug == "search":

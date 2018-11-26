96b2d9bf0f00e603cbe1401ce4d7f510417a439e
Christopher Boland <chris.boland97@gmail.com>
2018-11-26 17:53:03 +0000

    Now plots points on an actual map

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,47 @@
 lat = []
 lng = []
 sizes = []
+
 geocodes.forEach(function(object) {
   lat.push(object.lat);
   lng.push(object.lng);
   sizes.push(parseInt(object.count)*5)
 });
 
+var data = [{
+  type:'scattermapbox',
+  lat: lat,
+  lon: lng,
+  mode: 'markers',
+  marker: {
+    size: sizes
+  }
+}]
+
+console.log(data);
+
+
+var layout = {
+  autosize: true,
+  hovermode:'closest',
+  mapbox: {
+    bearing:0,
+    center: {
+      lat:56.4907,
+      lon:-4.2026
+    },
+    pitch:0,
+    zoom:5
+  },
+}
+
+
+Plotly.setPlotConfig({
+  mapboxAccessToken: 'pk.eyJ1IjoiY2hyaXNiOTcxOSIsImEiOiJjam95aWQ4YncyYzU0M3BsazVqMjZlbHlwIn0.lnsr5Pdz83SXN6kca0Slbw'
+})
+
+Plotly.plot('graph', data, layout)
+/**
 console.log(sizes)
 
 var data = [{
@@ -15,7 +50,7 @@ var data = [{
     lon: lng,
     zmin: 0,
     zmax: 17000,
-    /**colorscale: [
+    *colorscale: [
         [0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'],
         [0.4, 'rgb(188,189,220)'], [0.6, 'rgb(158,154,200)'],
         [0.8, 'rgb(117,107,177)'], [1, 'rgb(84,39,143)']
@@ -23,11 +58,12 @@ var data = [{
     colorbar: {
         title: 'Millions USD',
         thickness: 0.2
-    },*/
+    },
     marker: {
         size: sizes,
+        color: 'blue',
         line: {
-            color: 'black',
+            color: 'blue',
             width: 2
         },
     }
@@ -43,4 +79,14 @@ var layout = {
     }
 };
 
+
+data = graph_objs.Data([
+    graph_objs.Scattermapbox(
+        lat=['45.5017'],
+        lon=['-73.5673'],
+        mode='markers',
+    )
+])
+
 Plotly.plot(graph, data, layout);
+*/

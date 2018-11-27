475e2d27e340413fe8e531d92ed89aa6c51ac7d8
Kieran <kieran12lamb@live.com>
2018-11-27 17:48:40 +0000

    More months

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ prescriptionURL='https://www.opendata.nhs.scot/api/3/action/datastore_search?res
 prescriptionURLs = { 'August':"https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=592f3b58-2da8-4cc7-b4ab-e0eb0f16aa3f&q=",
                      'July':"https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=fec5dfdb-96dc-4e87-b7a0-6ecf20ec86e9&q=",
                      'June':"https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=d73cfc66-3330-416f-8507-873db28eca5c&q=",
+                     'April':'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=e35aebd7-55be-4c8b-82b7-54d1ed946b91&q=',
+                     'March':'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=0b7bc1dd-9731-405b-a55e-656a8c996a58&q=',
+                     'February':'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=1ae16a5f-aa26-4911-989b-ba4a6ab87037&q=',
+                     'January':'https://www.opendata.nhs.scot/api/3/action/datastore_search?resource_id=82ef2c55-5a31-4759-ab9a-83b176e107f2&q='
                     }
 
 
